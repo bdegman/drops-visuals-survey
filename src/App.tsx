3dadac7be6bdc0b132aa62daa5f12c3d3ade0114
Brian Degman <bdegman@roonlabs.com>
2025-08-28 15:02:59 -0500

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { z } from 'zod';
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? "/drops_webapp/" : "/",
+  base: process.env.NODE_ENV === 'production' ? "/drops-visuals-survey/" : "/",
 })
